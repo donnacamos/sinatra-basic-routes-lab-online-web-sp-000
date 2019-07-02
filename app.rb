62de922a19a4521b6f9647165bc7e138326d0f56
Donna Amos <donnacamos.developer@gmail.com>
2019-07-02 02:54:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,15 @@ class App < Sinatra::Base
   get '/' do 
    "Hello, World!"
   end 
+  
+  get '/name'
+  "My name is Donna."
+  end
+  
+  get '/hometown'
+  "My hometown is Albany."
+  end 
+  
+  get '/favorite-song'
+  "My favorite song is "
 end
