68c4c006a64fc15c18a3b6fb79dac9d3cd70771e
Donna Amos <donnacamos.developer@gmail.com>
2019-07-02 02:51:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do 
-    "My name is __" 
-    "My hometown is __"
-    "My favorite song is __"
+   "Hello, World!"
   end 
 end
